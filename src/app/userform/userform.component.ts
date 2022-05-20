3c32eac2517db33eae41ffa4c9c2b7456d2a7f6a
ngactive99 <ngactive99@gmail.com>
2022-05-20 10:27:50 +0530

    both ways sorting done

--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -11,6 +11,7 @@ export class UserformComponent implements OnInit {
   
   user: User = new User()
   users: User[]= []
+  order: boolean= true;
 
   save() {
     // console.log("the name of the user is " + this.user.name + " age : "+ this.user.age + "who's gender is : "+ this.user.gender);
@@ -28,9 +29,10 @@ export class UserformComponent implements OnInit {
   }
 
   sort() {
-    this.users.sort(function(user1, user2) {
-      return user1.age - user2.age;
-    })
+    this.users.sort((user1, user2) => {
+      return (this.order)? user1.age - user2.age: user2.age- user1.age;
+    }); this.order = !this.order;
+
   }
   constructor(public userService: UserService) {}
 
